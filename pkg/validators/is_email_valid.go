2d288171a6c417d50e23388c9e6d130b6239a72d
bruce-mig <h180009t@hit.ac.zw>
2023-10-20 14:49:03 +0200

    fixed regex

--- pkg/validators/is_email_valid.go
+++ pkg/validators/is_email_valid.go
@@ -3,9 +3,10 @@ package validators
 import "regexp"
 
 func IsEmailValid(email string) bool {
-	var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$") //check regexp documentation
+	var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{1,64}@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$") //check regexp documentation
 
-	if len(email) < 3|len(email) > 254 || !rxEmail.MatchString(email) {
+	if len(email) < 3 || len(email) > 254 || !rxEmail.MatchString(email) {
 		return false
 	}
+	return true
 }
