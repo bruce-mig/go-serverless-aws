19db008371d2b402c1e38356bac518a07aac7108
bruce-mig <h180009t@hit.ac.zw>
2023-10-20 15:41:56 +0200

    fixed header response

--- pkg/handlers/api_response.go
+++ pkg/handlers/api_response.go
@@ -2,11 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 )
 
-func apiResponse(status int, body interface{}) (*events.APIGatewayProxyResponse, error){
-	resp := events.APIGatewayProxyResponse{Headers : map[string]string["Content-Type":"application/json"]}
+func apiResponse(status int, body interface{}) (*events.APIGatewayProxyResponse, error) {
+	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
 	resp.StatusCode = status
 
 	stringBody, _ := json.Marshal(body)
